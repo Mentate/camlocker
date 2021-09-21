5c7bc1487d04a5964ea012267a6a7d2bafa75b81
mentate <josiah@josiahcarpenter.com>
2021-09-21 09:44:04 -0500

    Fixed the error handling when the camera is in use by another app

--- camlocker.py
+++ camlocker.py
@@ -1,12 +1,11 @@
 import cv2
 import time
-import win32api
+import win32api #seems like this is included with the windows python installer sometimes? I had to install it so I added it to the requirements.txt to be safe
 import ctypes
 
 
-#for some reason the lbp file didn't come with opencv and had to be downloaded. because of this I had to put the absolute path in, which is gross
+#for some reason the lbp file didn't come with opencv and had to be downloaded. because of this I had to put the path in, which is gross
 frontfaceCascade = cv2.CascadeClassifier(r'lbpcascade_frontalface.xml')
-
 
 
 #there's no point in checking for facial presense if the keyboard and mouse are being used.
@@ -15,19 +14,20 @@ def getIdleTime():
     return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
 
 #this checks for a face. If no face is found it checks once a second for five seconds
-# If no faces are found the entire 5 seconds it locks the machine
+# If no faces are found during the entire 5 seconds it returns False. If it finds a face it returns True
 def checkcam():
     #we don't want to capture the camera all the time. That would lock other apps out of it
     cap = cv2.VideoCapture(0)
     
     #if another app has control of the camera, base image will be None.
-    #That means the user is using the webcam for another app and we probably shouldn't lock the machine
+    #That means the user is using the webcam for another app and we probably shouldn't lock the machine anyways
+    rc, baseImage = cap.read()
     try:
-        rc, baseImage = cap.read()
-    except:
+        #if the base image is none the line below throws an error. This is how we know the webcam is being used by something else
+        gray = cv2.cvtColor(baseImage,cv2.COLOR_BGR2GRAY)      
+    except:        
         cap.release()
         return True
-    gray = cv2.cvtColor(baseImage,cv2.COLOR_BGR2GRAY)
     frontfaces = frontfaceCascade.detectMultiScale(gray,1.05,2)
     if(len(frontfaces) == 0):
         i = 0
